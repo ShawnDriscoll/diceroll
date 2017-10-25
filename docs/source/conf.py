05a286ad02829f6f14798c92e171da4793edde43
ShawnDriscoll <shawndriscoll@hotmail.com>
2017-10-25 14:01:20 -0700

    new PDF build

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -43,9 +43,9 @@ copyright = u'2017, Shawn Driscoll'
 # other places throughout the built documents.
 #
 # The short X.Y version.
-version = '2.3'
+version = '2.4'
 # The full version, including alpha/beta/rc tags.
-release = '2.3.2b'
+release = '2.4.0b'
 
 # There are two options for replacing |today|: either, you set today to some
 # non-false value, then it is used:
@@ -94,7 +94,7 @@ else:
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
-html_title = 'diceroll 2.3 Operations Manual'
+html_title = 'diceroll 2.4 Operations Manual'
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
 #html_short_title = None
