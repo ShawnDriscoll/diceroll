1290ce7c79c4e2de2c4e9bf252a6b9b758547eda
ShawnDriscoll <shawndriscoll@hotmail.com>
2019-11-11 22:53:43 -0800

    v3.0 docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -98,7 +98,7 @@ html_theme = 'agogo'
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
-html_title = 'diceroll 2.4 Operations Manual'
+html_title = 'diceroll 3.0 Operations Manual'
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
 #html_short_title = None
